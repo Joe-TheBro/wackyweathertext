1aa7010849b772d9f759bf5b1f99bf97764a95c8
clabbbbbbb <tami.nguyen172@gmail.com>
2024-07-14 01:02:19 -0500

    Packages are cooking

---
+++ dawgggg.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/piprate/json-gold/ld"
+	"log"
+	"os"
+)
+
+func main() {
+
+}
+
+/*type Weather interface {
+	renderAsciiArt() string
+}
+
+type Sunny struct {
+}*/
---
+++ go.mod
@@ -0,0 +1,10 @@
+module wackyweathertext
+
+go 1.22.5
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/piprate/json-gold v0.5.0
+)
+
+require github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
